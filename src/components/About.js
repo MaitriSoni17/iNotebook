8436612fa5e5cf4c454ea857b03a8282934df7d0
MaitriSoni17 <maitrisoni171@gmail.com>
2025-06-24 15:49:21 +0530

    Context API

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,15 @@
-import React from 'react'
-
+import React, { useContext, useEffect } from 'react'
+import noteContext from '../context/notes/noteContext'
 export const About = () => {
+  const a = useContext(noteContext)
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line
+  }, [])
+  
   return (
     <div>
-        This is About Page
+        This is About Page {a.state.name}
     </div>
   )
 }
