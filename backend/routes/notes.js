ec667eab3c4f39da608f99f07f32bd1f3323fead
MaitriSoni17 <maitrisoni171@gmail.com>
2025-06-24 14:03:55 +0530

    Created Update Note End Point

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -37,6 +37,37 @@ router.post('/addnote', fetchuser, [
         console.error(error.message);
         res.status(500).send("Internal server error");
     }
+})
 
+// ROUT 3: Update an existing node using: PUT "/api/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    try {
+        const { title, description, tag } = req.body;
+        // Create a new note object
+        const newNote = {};
+        if(title){
+            newNote.title = title;
+        }
+        if(description){
+            newNote.description = description;
+        }
+        if(tag){
+            newNote.tag = tag;
+        }
+        // Find the note to be updated and update it.
+        let note = await Note.findById(req.params.id);
+        if(!note){
+            return res.status(404).send("Not Found");
+        }
+        if(note.user.toString() !== req.user.id){
+            return res.status(401).send("Not Allowed");
+        }
+        note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
+        res.json({note});
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error");
+    }
 })
 module.exports = router
