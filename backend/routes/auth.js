c219c4e8de11b5351f1bd90df231564c3806e007
MaitriSoni17 <maitrisoni171@gmail.com>
2025-06-25 19:39:04 +0530

    Login Created

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -57,6 +57,7 @@ router.post('/login', [
     // body('password', 'Password must be atleat 5 characters').isLength({ min: 5 }),
 
 ], async (req, res) => {
+    let success = false;
     // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -66,11 +67,13 @@ router.post('/login', [
     try {
         let user = await User.findOne({ email });
         if (!user) {
-            return res.status(400).json({ error: "Please try to login with correct credentials." });
+            success = false;
+            return res.status(400).json({ success, error: "Please try to login with correct credentials." });
         }
         const passwordCompare = await bcrypt.compare(password, user.password);
         if (!passwordCompare) {
-            return res.status(400).json({ error: "Please try to login with correct credentials." });
+            success = false;
+            return res.status(400).json({ success, error: "Please try to login with correct credentials." });
         }
         const data = {
             user: {
@@ -78,7 +81,8 @@ router.post('/login', [
             }
         }
         const authtoken = jwt.sign(data, JWT_SECRET);
-        res.json({ authtoken })
+        success = true;
+        res.json({ success, authtoken })
     }
     catch (error) {
         console.error(error.message);
